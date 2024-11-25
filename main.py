5f7e91b25067b6d9c9a1fbdde549e938fe0f22d3
Seeker4264 <sebashmorales@gmail.com>
2024-11-25 16:07:27 -0400

    validaciones y lectura de txt

--- main.py
+++ main.py
@@ -1,23 +1,48 @@
 import claseMaquina
 
-initial_state = "q1",
-accepting_states = ["qf"],
-transition_function = {
+estado_inicial_ = "q1",
+estados_aceptados = ["qf"],
+transiciones = {
   ("q1","0"):("q1", "1", "R"),
   ("q1","1"):("q1", "0", "R"),
   ("q1"," "):("qf", "", "R"),
 }
-final_states = {"qf"}
+estado_final_ = {"qf"}
 
-t = claseMaquina.MaquinaTuring("1101000", 
-                  estado_inicial = "q1",
-                  estados_finales = final_states,
-                  transiciones=transition_function)
+def validarBinario(binario):
+  strBinario = str(binario)
 
-print("Dato de entrada en la cinta:" + t.get_tape() + "\n" + "\n" + t.get_tape() + " -> " + "q1")
+  for numero in strBinario:
+    if numero not in '01':
+      return False
 
-while not t.final():
-  t.step()
+  if (len(list(strBinario)) == 0) or (len(list(strBinario)) > 200):
+    return False
 
-print("\n" + "Resultado del cálculo de la máquina de Turing:")
-print(t.get_tape())
+  return True
+
+def main():
+  numeroBin = ""
+
+  with open('test.txt', 'r') as f:
+    numeroBin = f.read().replace("\n", "")
+  
+  if not validarBinario(numeroBin):
+    print("El dato de entrada debe ser un número binario, de entre 1 y 200 caractéres, pero se introdujo:\n" + str(numeroBin))
+    return
+
+  t = claseMaquina.MaquinaTuring(numeroBin, 
+                    estado_inicial = "q1",
+                    estados_finales = estado_final_,
+                    transiciones = transiciones)
+
+  print("Dato de entrada en la cinta: " + t.get_tape() + "\n" + "\n" + t.get_tape() + " -> " + "q1")
+
+  while not t.final():
+    t.step()
+
+  print("\n" + "Resultado del cálculo de la máquina de Turing:")
+  print(t.get_tape())
+  return
+
+main()
